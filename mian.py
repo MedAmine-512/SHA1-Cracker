cd95b59b819ce5260b552d730ef78cd2b2d1289a
MedAmine-512 <mohamedaminebenfakir@gmail.com>
2025-06-27 21:16:50 +0100

    improvement to the code using args  but nit completed yet

--- mian.py
+++ mian.py
@@ -1,17 +1,39 @@
 import hashlib
+import argparse
+import os
 
 
 
-def main():
-    sha1 = input("The SHA1 To Crack : ")
+
+def crack(sha1, wordlist):
+    if not os.path.isfile(wordlist):
+        print(f"[!] There is no file ")
+
+
     cleaned_sha1 = sha1.strip().lower()
-    with open('./rockyou.txt') as passwods:
+
+
+    with open(wordlist) as passwods:
         for line in passwods:
             passwod = line.strip()
             passwod_sha1 = hashlib.sha1( passwod.encode() ).hexdigest()
             if cleaned_sha1 == passwod_sha1:
-                print("The password is : ", passwod)
-                return
+                print("[+] The password is : ", passwod)
+                break
+
+
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description='SHA1 Password Cracker using a dictionary attack')
+    parser.add_argument('hash', help='The hash to crack')
+    parser.add_argument('-w', '--wordlist', default='./rockyou.txt', help="The path to the wordlist used for the cracking (default is rockyou.txt)")
+    
+    args = parser.parse_args()
+
+
+    crack(args.hash, args.wordlist)
             
         
 
